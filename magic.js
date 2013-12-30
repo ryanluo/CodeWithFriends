e1265fcf7fd5d42a9c1f4eb3a3141017b78f8493
usuallyacorn <rllluo@gmail.com>
2013-12-29 22:22:09 -0800

    moved folders to root

---
+++ magic.js
@@ -0,0 +1,79 @@
+var json1 = {"B3z":{"a":"-JBcEYtXqkURXyg08ojV","o":[8,"w"]},"B4d":{"a":"-JBcEYtXqkURXyg08ojV","o":[68,-1]},"B4m":{"a":"-JBcEYtXqkURXyg08ojV","o":[59,-1]},"B4v":{"a":"-JBcEYtXqkURXyg08ojV","o":[69,"w "]},"B4I":{"a":"-JBcEYtXqkURXyg08ojV","o":[35,"ok"]},"B4R":{"a":"-JBcEYtXqkURXyg08ojV","o":[48,"to"]},"B47":{"a":"-JBcEYtXqkURXyg08ojV","o":[22,"m"]},"B41":{"a":"-JBcEYtXqkURXyg08ojV","o":[11,"n"]},"B4C":{"a":"-JBcEYtXqkURXyg08ojV","o":[29,"l"]},"B4L":{"a":"-JBcEYtXqkURXyg08ojV","o":[39,"o"]},"B3t":{"a":"-JBcEYtXqkURXyg08ojV","o":["H"]},"B4U":{"a":"-JBcEYtXqkURXyg08ojV","o":[53,"t "]},"B4p":{"a":"-JBcEYtXqkURXyg08ojV","o":[59,"o"]},"B4g":{"a":"-JBcEYtXqkURXyg08ojV","o":[65,-1]},"B4F":{"a":"-JBcEYtXqkURXyg08ojV","o":[32," "]},"B44":{"a":"-JBcEYtXqkURXyg08ojV","o":[18,"a"]},"B4O":{"a":"-JBcEYtXqkURXyg08ojV","o":[44,"e"]},"B4x":{"a":"-JBcEYtXqkURXyg08ojV","o":[72,"ay"]},"B4o":{"a":"-JBcEYtXqkURXyg08ojV","o":[58,"l"]},"B43":{"a":"-JBcEYtXqkURXyg08ojV","o":[16,"s "]},"B4r":{"a":"-JBcEYtXqkURXyg08ojV","o":[61,"olol"]},"B4i":{"a":"-JBcEYtXqkURXyg08ojV","o":[63,-1]},"B3v":{"a":"-JBcEYtXqkURXyg08ojV","o":[2," "]},"B4N":{"a":"-JBcEYtXqkURXyg08ojV","o":[42,"ne"]},"B4W":{"a":"-JBcEYtXqkURXyg08ojV","o":[57,"t ll"]},"B4E":{"a":"-JBcEYtXqkURXyg08ojV","o":[31,"l"]},"B3y":{"a":"-JBcEYtXqkURXyg08ojV","o":[7,"a"]},"B4Q":{"a":"-JBcEYtXqkURXyg08ojV","o":[46,"s "]},"B4Z":{"a":"-JBcEYtXqkURXyg08ojV","o":[63,"ol"]},"B4H":{"a":"-JBcEYtXqkURXyg08ojV","o":[34,"o"]},"B46":{"a":"-JBcEYtXqkURXyg08ojV","o":[20,"na"]},"B4u":{"a":"-JBcEYtXqkURXyg08ojV","o":[68,"e"]},"B4l":{"a":"-JBcEYtXqkURXyg08ojV","o":[60,-1]},"B4c":{"a":"-JBcEYtXqkURXyg08ojV","o":[67,"ol"]},"B4f":{"a":"-JBcEYtXqkURXyg08ojV","o":[66,-1]},"B4T":{"a":"-JBcEYtXqkURXyg08ojV","o":[52,"e"]},"B4B":{"a":"-JBcEYtXqkURXyg08ojV","o":[28," "]},"B40":{"a":"-JBcEYtXqkURXyg08ojV","o":[9,"re"]},"B4K":{"a":"-JBcEYtXqkURXyg08ojV","o":[38,"m"]},"B49":{"a":"-JBcEYtXqkURXyg08ojV","o":[25,"a"]},"B4S":{"a":"-JBcEYtXqkURXyg08ojV","o":[50," g"]},"B4w":{"a":"-JBcEYtXqkURXyg08ojV","o":[71,"g"]},"B4n":{"a":"-JBcEYtXqkURXyg08ojV","o":[58,-1]},"B4A":{"a":"-JBcEYtXqkURXyg08ojV","o":[26,"nd"]},"B4J":{"a":"-JBcEYtXqkURXyg08ojV","o":[37," "]},"B4e":{"a":"-JBcEYtXqkURXyg08ojV","o":[67,-1]},"B3u":{"a":"-JBcEYtXqkURXyg08ojV","o":[1,"i"]},"B4V":{"a":"-JBcEYtXqkURXyg08ojV","o":[55,"ou"]},"B42":{"a":"-JBcEYtXqkURXyg08ojV","o":[12,"ce i"]},"B4D":{"a":"-JBcEYtXqkURXyg08ojV","o":[30,"o"]},"B4M":{"a":"-JBcEYtXqkURXyg08ojV","o":[40,"m "]},"B4q":{"a":"-JBcEYtXqkURXyg08ojV","o":[60,"l"]},"B4h":{"a":"-JBcEYtXqkURXyg08ojV","o":[64,-1]},"B45":{"a":"-JBcEYtXqkURXyg08ojV","o":[19," "]},"B4t":{"a":"-JBcEYtXqkURXyg08ojV","o":[67," "]},"B3x":{"a":"-JBcEYtXqkURXyg08ojV","o":[6,"L"]},"B4Y":{"a":"-JBcEYtXqkURXyg08ojV","o":[62,"l"]},"B4b":{"a":"-JBcEYtXqkURXyg08ojV","o":[66,"l"]},"B4G":{"a":"-JBcEYtXqkURXyg08ojV","o":[33,"l"]},"B4P":{"a":"-JBcEYtXqkURXyg08ojV","o":[45,"d"]},"B4k":{"a":"-JBcEYtXqkURXyg08ojV","o":[61,-1]},"B48":{"a":"-JBcEYtXqkURXyg08ojV","o":[23,"e "]},"B4a":{"a":"-JBcEYtXqkURXyg08ojV","o":[65,"o"]},"B3w":{"a":"-JBcEYtXqkURXyg08ojV","o":[3,"My "]},"B4X":{"a":"-JBcEYtXqkURXyg08ojV","o":[61,"o"]},"B4s":{"a":"-JBcEYtXqkURXyg08ojV","o":[65,"ol"]},"B4j":{"a":"-JBcEYtXqkURXyg08ojV","o":[62,-1]}}
+function merge(left, right){
+    var result  = [],
+        il      = 0,
+        ir      = 0;
+
+    while (il < left.length && ir < right.length){
+        if (left[il] < right[ir]){
+            result.push(left[il++]);
+        } else {
+            result.push(right[ir++]);
+        }
+    }
+
+    return result.concat(left.slice(il)).concat(right.slice(ir));
+}
+
+function mergeSort(items){
+
+    // Terminal case: 0 or 1 item arrays don't need sorting
+    if (items.length < 2) {
+        return items;
+    }
+
+    var middle = Math.floor(items.length / 2),
+        left    = items.slice(0, middle),
+        right   = items.slice(middle);
+
+    return merge(mergeSort(left), mergeSort(right));
+}
+
+var shift = function(list){
+	for(var i = list.length; i>0; i--){
+		list[i]=list[i-1]
+	}
+	list[0] = 0
+}
+var normalize = function(obj){
+	if(typeof obj[0]=='number'&& obj[0] > 0){
+
+	} else{
+		shift(obj)
+	}
+	if(!obj[2]){
+		obj[2]=0
+	}
+}
+
+var reconstruct = function(dict){
+	keys = []
+	for(var key in dict){
+		keys.push(key)
+		normalize(dict[key].o)
+	}
+	keys = mergeSort(keys);
+	for(var key in keys){		
+	//	console.log(dict[keys[key]].o)
+	}
+	var write = ""
+	counter=0;
+	for(var key in keys){
+		var input = dict[keys[key]].o
+		if(typeof input[1]=='number'){
+			if(input[2]==0)
+				write = write.substring(0,write.length+input[1])
+			else
+				write = write.substring(0,input[0])+write.substring(input[0]+(-1*input[1]));
+		}
+		else 
+			write = [write.slice(0,input[0]),input[1],write.slice(input[0])].join('')
+		counter++
+		//if(counter>=50&&counter<100)
+			console.log(write)
+	}
+	return write
+}
+
+
+console.log(reconstruct(json1));
